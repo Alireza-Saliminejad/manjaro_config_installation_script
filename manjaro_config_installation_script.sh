ed4420309726c5b1cdb035fedbe8fe6d16c0ce7a
ali <alireza.saliminejad.97@gmail.com>
2023-04-04 15:10:00 +0330

    minor change

--- manjaro_config_installation_script.sh
+++ manjaro_config_installation_script.sh
@@ -9,7 +9,7 @@ putgitrepo() {
 	[ -z "$3" ] && branch="main" || branch="$repobranch"
 	dir=$(mktemp -d)
 	[ ! -d "$2" ] && mkdir -p "$2"
-	chown "$name":sudo "$dir" "$2"
+	chown "$name":wheel "$dir" "$2"
 	sudo -u "$name" git -C "$repodir" clone --depth 1 \
 		--single-branch --no-tags -q --recursive -b "$branch" \
 		--recurse-submodules "$1" "$dir"
